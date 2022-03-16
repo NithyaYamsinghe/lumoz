0c773200e509dc0a12aabeab7b10d4d6cb3e4983
nithyayamsinghe <nitheeromeshi@gmail.com>
2022-03-17 01:56:57 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/lumoz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lumoz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: const [
+          Text("theme data",
+          style: TextStyle(
+            fontSize: 30
+          ),)
+        ],
+      ),
+    );
+  }
+}
